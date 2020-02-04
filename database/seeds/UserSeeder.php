a6fa26412c1ed2f5d07d542a69406a1742615176
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-04 13:15:09 +0000

    add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Ricardo Nainggolan',
+                'email' => 'ricardo.nainggolan@arnocindonesia.com',
+                'password' => Hash::make('123456')
+            ]
+        ];
+
+        foreach($users as $user) {
+            User::create($user);
+        }
+    }
+}
