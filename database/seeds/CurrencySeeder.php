e03c00c002bf3dda88b707a2b5d2bb93b591d30e
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-04 10:52:15 +0000

    add Currency model & seeders

---
+++ database/seeds/CurrencySeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $currencies = [
+            [
+                'code' => 'IDR',
+                'name' => 'Indonesian Rupiah'
+            ],
+            [
+                'code' => 'SGD',
+                'name' => 'Singapore Dollar'
+            ],
+            [
+                'code' => 'USD',
+                'name' => 'American Dollar'
+            ],
+            [
+                'code' => 'EUR',
+                'name' => 'Euro'
+            ],
+        ];
+
+        foreach($currencies as $currency) {
+            DB::table('currencies')->insert([
+                'code' => $currency['code'],
+                'name' => $currency['name'],
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]);
+        }
+    }
+}
