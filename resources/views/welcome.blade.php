10b7e93891bd1d5d402300dc6f4d0cbef1fb8999
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-04 10:36:38 +0000

    change default title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Arnoc Indonesia Energi</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -84,18 +84,7 @@
 
     <div class="content">
       <div class="title m-b-md">
-        Expenses Claim
-      </div>
-
-      <div class="links">
-        <a href="https://laravel.com/docs">Docs</a>
-        <a href="https://laracasts.com">Laracasts</a>
-        <a href="https://laravel-news.com">News</a>
-        <a href="https://blog.laravel.com">Blog</a>
-        <a href="https://nova.laravel.com">Nova</a>
-        <a href="https://forge.laravel.com">Forge</a>
-        <a href="https://vapor.laravel.com">Vapor</a>
-        <a href="https://github.com/laravel/laravel">GitHub</a>
+        Arnoc Indonesia Energi
       </div>
     </div>
   </div>
