aca629aefb1b70c6723952dc91f7c1afe295cf8e
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-11 07:10:21 +0000

    update expense claims show blade

---
+++ resources/views/expense-claims/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-3">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="{{ route('expense-claims.index') }}">My Claims</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('expense-claims.create') }}">Make Claim</a>
+        </li>
+      </ul>
+    </div>
+    <div class="col-md-9">
+      <div class="card">
+        <div class="card-header">{{ $expenseClaim->code }}</div>
+        <div class="card-body">
+          Expense Claim details showed here. TBD.
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
