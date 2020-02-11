c99b4e43b44ce9d9fd8905c8f85b090fb336351e
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-11 04:24:43 +0000

    add getExpenseClaimsById service

--- app/Services/ExpenseClaimService.php
+++ app/Services/ExpenseClaimService.php
@@ -7,6 +7,28 @@ use App\Repositories\ExpenseClaimRepository;
 
 class ExpenseClaimService
 {
+    public function getExpenseClaims()
+    {
+        $expenseClaims = ExpenseClaim::all();
+
+        return $expenseClaims;
+    }
+
+    public function getExpenseClaimsById($userId)
+    {
+        $expenseClaims = ExpenseClaim::where('user_id', $userId)
+            ->get();
+
+        return $expenseClaims;
+    }
+
+    public function getExpenseClaim($id)
+    {
+        $expenseClaim = ExpenseClaim::find($id);
+
+        return $expenseClaim;
+    }
+
     public function createExpenseClaim(Array $data)
     {
         $expenseClaimRepository = new ExpenseClaimRepository();
