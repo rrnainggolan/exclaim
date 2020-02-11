3fc12129f8302b490c3b5d4ef8847f3b64c575d5
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-11 07:48:04 +0000

    add expense_claims_approved table

---
+++ database/migrations/2020_02_11_074313_create_expense_claims_approved_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpenseClaimsApprovedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_claims_approved', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->bigInteger('expense_claim_id')->unsigned()->index();
+            $table->foreign('expense_claim_id')->references('id')->on('expense_claims');
+
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expense_claims_approved');
+    }
+}
