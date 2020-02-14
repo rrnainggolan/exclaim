0531a868d9ef68fdd1d6ed2450dda1d533761624
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-02-14 16:08:04 +0000

    add dynamic add & remove expenses

--- resources/views/expense-claims/create.blade.php
+++ resources/views/expense-claims/create.blade.php
@@ -60,44 +60,48 @@
               </div>
             </div>
 
-            <div class="form-group form-row">
-              <div class="col-auto">
-                {{ Form::select('expenses[0][type]', $expenseTypes, '', [
-                  'placeholder' => 'Choose expense type',
-                  'class' => 'custom-select'
-                ]) }}
-              </div>
-              <div class="col-auto">
-                <div class="input-group mb-2">
-                  <div class="input-group-prepend">
-                      <div class="input-group-text">Rp.</div>
-                      {{ Form::hidden('expenses[0][currency]', 1) }}
+            <div class="expenses">
+              <p>Expenses: <a href="javascript:void(0);" id="addExpense">Add more expenses</a></p>
+              <div class="expense-0">
+                <div class="form-group form-row">
+                  <div class="col-auto">
+                    {{ Form::select('expenses[0][type]', $expenseTypes, '', [
+                      'placeholder' => 'Choose expense type',
+                      'class' => 'custom-select'
+                    ]) }}
                   </div>
-                  {{ Form::text('expenses[0][amount]', '', [
-                  'placeholder' => 'Input the amount',
-                  'class' => 'form-control'
-                  ]) }}
+                  <div class="col-auto">
+                    <div class="input-group mb-2">
+                      <div class="input-group-prepend">
+                          <div class="input-group-text">Rp.</div>
+                          {{ Form::hidden('expenses[0][currency]', 1) }}
+                      </div>
+                      {{ Form::text('expenses[0][amount]', '', [
+                      'placeholder' => 'Input the amount',
+                      'class' => 'form-control'
+                      ]) }}
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    {{ Form::file('expenses[0][file][]', $attributes = [
+                      'id' => 'attachments0',
+                      'class' => 'custom-file-input',
+                      'multiple' => 'multiple',
+                    ]) }}
+                    {{ Form::label('attachments0', 'Attachments', [
+                      'class' => 'custom-file-label'
+                    ]) }}
+                  </div>
+                  <div class="col-9">
+                    {{ Form::textarea('expenses[0][remarks]', '', [
+                      'rows' => 4,
+                      'placeholder' => 'Type remarks if any',
+                      'class' => 'form-control'
+                    ]) }}
+                  </div>
                 </div>
               </div>
-              <div class="col-auto">
-                {{ Form::file('expenses[0][file][]', $attributes = [
-                  'id' => 'attachments1',
-                  'class' => 'custom-file-input',
-                  'multiple' => 'multiple',
-                ]) }}
-                {{ Form::label('attachments1', 'Attachments', [
-                  'class' => 'custom-file-label'
-                ]) }}
-              </div>
-              <div class="col-9">
-                {{ Form::textarea('expenses[0][remarks]', '', [
-                  'rows' => 4,
-                  'placeholder' => 'Type remarks if any',
-                  'class' => 'form-control'
-                ]) }}
-              </div>
             </div>
-
 
           {{ Form::submit('Submit!', ['class' => 'btn btn-primary']) }}
           {{ Form::close() }}
@@ -127,9 +131,9 @@
     });
 
     // MULTIPLE FILE INPUT 
-    $('#attachments1').on('change', function(e) {
+    $(document).on('change', '.custom-file-input', function(e) {
       var labelValue;
-      var files = $('#attachments1').prop("files");
+      var files = $(this).prop("files");
       var names = $.map(files, function(val) { 
         return val.name; 
       });
@@ -141,7 +145,51 @@
       }
 
       $(this).next('.custom-file-label').html(labelValue);
-    })
+    });
+
+    // ADD MULTIPLE EXPENSES
+    var x = 1;
+    var addExpense = $('#addExpense');
+    var wrapper = $('.expenses');
+    var expenseHTML;
+    var expenseTypes = @json($expenseTypes);
+
+    // on add expense click
+    addExpense.on('click', function() {
+      expenseHTML = '<div class="form-group form-row">';
+
+      // type
+      expenseHTML += '<div class="col-auto"><select class="custom-select" name="expenses[' + x + '][type]"><option selected="selected" value="">Choose expense type</option>';
+
+      Object.keys(expenseTypes).forEach(function(key) {
+        expenseHTML += '<option value="' + key + '">' + expenseTypes[key] + ' </option>';
+      });
+        
+      expenseHTML += '</option></select></div>';
+
+      // amount
+      expenseHTML += '<div class="col-auto"><div class="input-group mb-2"><div class="input-group-prepend"><div class="input-group-text">Rp.</div><input name="expenses[' + x +'][currency]" type="hidden" value="1"></div><input placeholder="Input the amount" class="form-control" name="expenses[' + x + '][amount]" type="text" value=""></div></div>';
+
+      // attachments
+      expenseHTML += '<div class="col-auto"><input id="attachments' + x + '" class="custom-file-input" multiple="multiple" name="expenses[' + x + '][file][]" type="file"><label for="attachments' + x + '" class="custom-file-label">Attachments</label></div>';
+
+      // remarks
+      expenseHTML += '<div class="col-9"><textarea rows="4" placeholder="Type remarks if any" class="form-control" name="expenses[' + x + '][remarks]" cols="50"></textarea></div>';
+
+      // remove expense link
+      expenseHTML += '<div class="col-auto"><a href="javascript:void(0);" class="remove-expense">Remove this expense</a></div>';
+      
+      expenseHTML += '</div>';
+
+      wrapper.append(expenseHTML);
+      x++;
+    });
+
+    // on remove expense click
+    $(wrapper).on('click', '.remove-expense', function(e){
+        e.preventDefault();
+        $(this).parent('div').parent('div').remove(); //Remove field html
+    });
 
   </script>
 @endpush
