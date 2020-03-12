96300c4eba261a3dda8f3368282c514174a58a13
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-03-12 09:10:08 +0000

    create notification for addes users

---
+++ app/Notifications/UserCreated.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\User;
+
+class UserCreated extends Notification
+{
+    use Queueable;
+
+    public $user;
+    private $password;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $message = new MailMessage;
+        $message->subject('Welcome to Arnoc Indonesia Energi')
+            ->greeting('Hello, '.$this->user->name.'!')
+            ->line('Here is your credential:')
+            ->line('Username: ' . $this->user->email)
+            ->line('Password: '.$this->password)
+            ->line('We strongly recommended you to change your password after you login.')
+            ->action('Click here to login & update your password', route('edit.password', ['id' => $this->user->id]))
+            ->line('Thank you for using our application!');
+
+        return $message;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
