a823d3179a49f0db7dcabdd1bbd08ddc5515c825
Ricardo Nainggolan <ricardo.nainggolan@arnocindonesia.com>
2020-03-12 09:11:10 +0000

    add new link in header for changing password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,9 @@
               </a>
 
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('edit.password', ['id' => Auth::user()->id]) }}">
+                  Change Password
+                </a>
                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                   {{ __('Logout') }}
